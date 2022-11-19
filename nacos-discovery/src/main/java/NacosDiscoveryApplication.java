5e748c87713555e8d5396fb288fa358743bb0e60
skyfrost126 <979628059@qq.com>
2022-11-19 17:26:05 +0800

    dependency update

---
+++ nacos-discovery/src/main/java/NacosDiscoveryApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosDiscoveryApplication.class, args);
+    }
+}
